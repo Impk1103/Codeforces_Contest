1e026fdfde646d67acf223e06d8ddb6757ba92c1
Impk1103 <impk1103@gmail.com>
2024-06-17 00:45:15 +0530

    Update Alice_and_Books.cpp

--- Round_953(Div.2)/Alice_and_Books.cpp
+++ Round_953(Div.2)/Alice_and_Books.cpp
@@ -1,25 +1,29 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-
+ 
 using namespace std;
-
+ 
 int main() {
     int t;
     cin >> t;
-
+ 
     while (t--) {
         int n;
         cin >> n;
-        int book[n];
-        for(int i=0;i<n;i++){
-            cin>>book[i];
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
         }
-        sort(book[0],book[n-1]);
-
-        int max=book[n-1]+ book[n-2];
-        cout<< max<< endl;
+        int max = 0;
+        for (int i = 0; i < n-1; i++) {
+            if(arr[i]>max){
+                max=arr[i];
+            }
+        }
+ 
+        cout << max+arr[n-1]<<endl;
     }
-
+ 
     return 0;
 }
