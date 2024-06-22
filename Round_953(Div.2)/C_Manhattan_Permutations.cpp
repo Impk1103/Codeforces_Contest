b874e31ce9a9ad7377e8f5b12cdc0dc1cb58606c
Impk1103 <impk1103@gmail.com>
2024-06-23 00:58:03 +0530

    prob c

--- Round_953(Div.2)/C_Manhattan_Permutations.cpp
+++ Round_953(Div.2)/C_Manhattan_Permutations.cpp
@@ -1,9 +1,29 @@
-#include<iostream>
-#include<algorithm>
+#include<bits/stdc++.h>
 using namespace std;
-
-int main()
-{
-    
-    return 0;
+#define ll long long
+#define MP make_pair
+mt19937 rnd(time(0));
+const int MAXN=2e5+5;
+ll n,k,a[MAXN];
+void solve(){
+	cin>>n>>k;
+	for(int i=1;i<=n;i++) a[i]=i;
+	if(k&1){cout<<"No\n";return;}
+	int l=1,r=n;
+	while(l<=r){
+		if(k>=2*(r-l)) swap(a[l],a[r]),k-=2*(r-l),l++,r--;
+		else r--;
+	}
+	if(k){
+		cout<<"No\n";
+	}else{
+		cout<<"Yes\n";
+		for(int i=1;i<=n;i++) cout<<a[i]<<" \n"[i==n];
+	}
+}
+int main(){
+	ios::sync_with_stdio(false);
+	int _;cin>>_;
+	while(_--) solve();
+	return 0;
 }
